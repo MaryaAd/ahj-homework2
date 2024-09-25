875df1c5851c4c2d2544dc2ce6c26287256adfd8
Marya Adaratskih <marya.adaratskih@yandex.ru>
2024-09-25 18:19:45 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import Game from "./game/game";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const game = new Game(document.querySelector(".game"));
+
+  setInterval(() => {
+    game.changePosition();
+    console.log("interval");
+  }, 1000);
+});
